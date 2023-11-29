20a7e6254f005a5cdae1bcd0d567d531bbd206a5
Advaita <148793147+01adv@users.noreply.github.com>
2023-11-29 14:03:13 +0000

    updating file

---
+++ 1_variable.js
@@ -0,0 +1,10 @@
+const accountID = 112432
+let accountEmail = "viksit@123"
+var accountPassword = '12345'
+let accoutCity = "Rudrapur"
+
+
+// prefer let over var
+
+// console.log(accountID)
+console.table([accountID, accountEmail, accoutCity])
---
+++ 2_datatypes.js
@@ -0,0 +1,7 @@
+'use strict' // treat all js code as newer version
+
+//code readability should be high
+
+// number => 2 to power 53
+//bigint very big number
+
